d6cdbc83dea4b772c9eba9c889107932e2d672fe
csallahbakash <csallahbakash@gmail.com>
2023-04-13 18:27:39 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Development! first test'
+    return 'Hello, World!'
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
