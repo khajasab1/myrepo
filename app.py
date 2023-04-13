e29cfedacb2489d1afb85295bf9b352e45dfc8af
csallahbakash <csallahbakash@gmail.com>
2023-04-13 17:40:03 +0530

    simplified version

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Development! first '
+    return 'Hello, Development! first test'
 
 if __name__=="__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=8080)
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Deploy the Docker image to App Engine Flexible
-gcloud app deploy --image-url gcr.io/khaja-dv/my-flask-app:$COMMIT_SHA
-
-# Cleanup Docker images
-docker images -q gcr.io/khaja-dv/my-flask-app:$COMMIT_SHA | xargs docker rmi
